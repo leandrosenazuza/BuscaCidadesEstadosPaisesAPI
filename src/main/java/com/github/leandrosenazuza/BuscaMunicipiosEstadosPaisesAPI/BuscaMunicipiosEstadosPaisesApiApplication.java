d11a2bcf379b4e90987cd058175ec10586af47a8
leandrosenazuza <leandrosenazuza@gmail.com>
2021-12-03 21:21:38 -0300

    Primeiro commit

---
+++ src/main/java/com/github/leandrosenazuza/BuscaMunicipiosEstadosPaisesAPI/BuscaMunicipiosEstadosPaisesApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.leandrosenazuza.BuscaMunicipiosEstadosPaisesAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscaMunicipiosEstadosPaisesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscaMunicipiosEstadosPaisesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/leandrosenazuza/BuscaMunicipiosEstadosPaisesAPI/BuscaMunicipiosEstadosPaisesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.leandrosenazuza.BuscaMunicipiosEstadosPaisesAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscaMunicipiosEstadosPaisesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
